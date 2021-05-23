278151d6ae633933944e3c65187e961a3e538f79
Tatiana Alekseeva <alekseeva@mercurydevelopment.com>
2021-05-24 00:19:24 +0400

    Add components Weather for one day & weather for week

--- src/components/WeatherDay/WeatherDay.tsx
+++ src/components/WeatherDay/WeatherDay.tsx
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../Card";
 import Select from "../Select";
 import DatePicker from "../DatePicker";
 import EmptyPlaceholder from "../EmptyPlaceholder";
-import WeatherCard from "../ WeatherCard";
+import WeatherCard from "../WeatherCard";
 import { WeatherDaySelectWrapper } from "./WeatherDay.style";
 
-const WeatherDay: React.FC = () => (
-  <Card title="Forecast for a Date in the Past">
-    <WeatherDaySelectWrapper>
-      <Select />
-      <DatePicker />
-    </WeatherDaySelectWrapper>
+const WeatherDay: React.FC = () => {
+  const [city, setCity] = useState<string | null>(null);
+  const [time, setTime] = useState<any | null>(null);
+  const [weather, setWeather] = useState(null);
 
-    <WeatherCard />
-    {/* <EmptyPlaceholder /> */}
-  </Card>
-);
+  useEffect(() => {
+    if (city && time) {
+      const [lat, lon] = city.split(",");
+
+      fetch(
+        `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat.trim()}&lon=${lon.trim()}&units=metric&dt=${time}&appid=b8cd2ec6c2ebdc9dcde7792dd120d62b`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setWeather(data.current);
+        });
+    }
+  }, [city, time]);
+
+  const handleChangeDate = (time: any) => {
+    const timestamp = new Date(time.split("-").join(".")).getTime() / 1000;
+    setTime(timestamp);
+  };
+
+  const handleChange = (city: string) => {
+    setCity(city);
+  };
+
+  return (
+    <Card title="Forecast for a Date in the Past">
+      <WeatherDaySelectWrapper>
+        <Select onChange={handleChange} />
+        <DatePicker onChange={handleChangeDate} />
+      </WeatherDaySelectWrapper>
+      {weather ? <WeatherCard data={weather} /> : <EmptyPlaceholder />}
+    </Card>
+  );
+};
 
 export default WeatherDay;
--- src/components/WeatherWeek/WeatherWeek.tsx
+++ src/components/WeatherWeek/WeatherWeek.tsx
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../Card";
 import Select from "../Select";
 import EmptyPlaceholder from "../EmptyPlaceholder";
-import WeatherCard from "../ WeatherCard";
+import WeatherCard from "../WeatherCard";
 import { CardWeatherCardWrapper } from "./WeatherWeek.style";
 
-const WeatherWeek: React.FC = () => (
-  <Card title="7 Days Forecast">
-    <Select />
-    <CardWeatherCardWrapper>
-      <WeatherCard isWeek={true} />
-      <WeatherCard isWeek={true} />
-      <WeatherCard isWeek={true} />
-    </CardWeatherCardWrapper>
+const WeatherWeek: React.FC = () => {
+  const [city, setCity] = useState<string | null>(null);
+  const [weather, setWeather] = useState([]);
 
-    {/* <EmptyPlaceholder /> */}
-  </Card>
-);
+  useEffect(() => {
+    if (city) {
+      const [lat, lon] = city.split(",");
+
+      fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat.trim()}&lon=${lon.trim()}&units=metric&appid=b8cd2ec6c2ebdc9dcde7792dd120d62b`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setWeather(data.daily);
+        });
+    }
+  }, [city]);
+
+
+
+  const handleChange = (city: string) => {
+    setCity(city);
+  };
+
+  return (
+    <Card title="7 Days Forecast">
+      <Select onChange={handleChange} />
+      {weather.length ? (
+        <CardWeatherCardWrapper>
+          {weather.map((data) => (
+            <WeatherCard isWeek={true} data={data} />
+          ))}
+        </CardWeatherCardWrapper>
+      ) : (
+        <EmptyPlaceholder />
+      )}
+    </Card>
+  );
+};
 
 export default WeatherWeek;
